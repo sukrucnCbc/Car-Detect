4d2a5f4cb4e7eaa8a281923b069805bbf81279dc
Şükrücan Cebeci <sukrucan.ieeektu@gmail.com>
2021-01-17 14:46:38 +0300

    Signed-off-by: Şükrücan Cebeci <sukrucan.ieeektu@gmail.com>

---
+++ tespit/denemecar.py
@@ -0,0 +1,24 @@
+import cv2
+import numpy as np
+
+
+video =cv2.VideoCapture("carxd.mp4")
+araba_bulucu =cv2.CascadeClassifier("cars.xml")
+
+while  True:
+    ret,kare=video.read()
+
+    griton=cv2.cvtColor(kare,cv2.COLOR_BGR2GRAY)
+
+    arabalar = araba_bulucu.detectMultiScale(griton,1.1,3)
+
+    for(x,y,w,h) in arabalar:
+        cv2.rectangle(kare,(x,y),(x+w,y+h),(255,0,0),3)
+    cv2.imshow("video", kare)
+
+
+    if cv2.waitKey(5) & 0xFF == ord('q'):
+       break
+
+video.release()
+cv2.destroyAllWindows()
